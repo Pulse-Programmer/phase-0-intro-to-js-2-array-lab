3403877651274da5c380efe31486e6796183d82f
Stephen Ochieng <materiamystic@gmail.com>
2024-03-18 22:40:32 +0300

    Adds the required functions

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+    const copycats = [...cats, name];
+    return copycats;
+}
+
+function prependCat(name) {
+    const copycats = [name, ...cats];
+    return copycats;
+}
+
+function removeLastCat() {
+    const copycats = cats.slice(0, -1);
+    return copycats;
+}
+
+function removeFirstCat() {
+    const copycats = cats.slice(1);
+    return copycats;
+}
